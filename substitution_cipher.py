d8b9a2cc0424b5a4be05ef1c1d309a720711ffbf
Jon Cooper <joncoop@users.noreply.github.com>
2018-02-13 12:35:15 -0500

    Add files via upload

---
+++ substitution_cipher.py
@@ -0,0 +1,15 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+key = "qwertyuioplkjhgfdsazxcvbnm "
+
+def encrypt(message):
+    pass
+
+def decrypt(message):
+    pass
+
+
+unencrypted_message = "encryption is fun"
+encrypted_message = encrypt(unencrypted_message)
+
+print(unencrypted_message)
+print(encrypted_message)
