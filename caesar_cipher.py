28dec008f7642819cdd690ea806a5bb5b3bc4dc9
Jon Cooper <joncoop@users.noreply.github.com>
2018-02-13 12:36:45 -0500

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,16 @@
+def shift(letter, n):
+    pass
+
+def encrypt(message, shift_amount):
+    pass
+
+
+def decrypt(message, shift_amount):
+    pass
+
+secret_message = "encryption is fun"
+encrypted_message = encrypt(secret_message, 3)
+
+print(encrypted_message)
+
+
