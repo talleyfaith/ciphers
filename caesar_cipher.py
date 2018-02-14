4559daa9c983478cef2257a6d25c1d723e6fd407
Faith Talley <talleyfaith@gmail.com>
2018-02-14 13:53:32 -0500

    Worked on caesar file

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,16 +1,41 @@
-def shift(letter, n):
-    pass
+def shift(letter, shift_amount):
+    unicode_value = ord(letter) + shift_amount
+    
+    if unicode_value > 126:     
+        new_letter = chr(unicode_value  -95)
+    else:
+        new_letter = chr(unicode_value)
+
+    return new_letter
 
 def encrypt(message, shift_amount):
-    pass
+    result = ""
+
+    for letter in message:
+        result += shift(letter, shift_amount)
+
+    return result
 
 
 def decrypt(message, shift_amount):
-    pass
+    result = ""
 
-secret_message = "encryption is fun"
-encrypted_message = encrypt(secret_message, 3)
+    for letter in message:
+        unicode_value = ord(letter) + shift_amount
+        if unicode_value - shift_amount < 32:
+            result += shift(letter, -shift_amount + 95)
+        else:
+            result += shift(letter, -shift_amount)
+        
 
+    return result
+
+unencrypted_message = "abcdwxyz"
+encrypted_message = encrypt(unencrypted_message, 10)
+decrypted_message = decrypt(encrypted_message, 10)
+
+print(unencrypted_message)
 print(encrypted_message)
+print(decrypted_message)
 
 
